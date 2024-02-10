b0972ed3c15a9c2febfb7b3771db4210f68adb97
Anthony <anthonylsa12@gmail.com>
2024-02-10 13:13:48 -0800

    Added CSS

--- views/default.jsx
+++ views/default.jsx
@@ -5,9 +5,13 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="./style.css"/>
             </head>
             <body>
                 {html.children}
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
             </body>
         </html>
     )
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -13,7 +13,7 @@ function index (data) {
     return (
         <Def>
             <main>
-                <h1>PLACES INDEX PAGE</h1>
+                <h1>Restaurants</h1>
                 {placesFormatted}
             </main>
         </Def>
